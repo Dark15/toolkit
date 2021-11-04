8bae3359b1532f471bf9955ff87ed0d7aea6e5e2
Starry <starrycat15@gmail.com>
2021-11-04 15:35:11 +0800

    chore(emotion): setup css prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import styled from '@emotion/styled';
-import './App.css';
+import './App.css'
 
+import styled from '@emotion/styled'
+
+const Text = () => {
+  return (
+    <h1
+      css={{
+        color: 'red'
+      }}
+    >
+      Hello World
+    </h1>
+  )
+}
 
 const Div = styled.div`
   color: red;
 `
 
 const App = () => {
-  return <Div>red</Div>
+  return <Text />
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()]
+  plugins: [
+    react({
+      babel: {
+        babelrc: true
+      }
+    }),
+    WindiCSS()
+  ]
 })

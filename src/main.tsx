71c513d8dd2dd9e0a666acecb36b2825de72c3b5
Dark15 <starrycat15@gmail.com>
2022-05-23 02:03:04 +0800

    refactor: Update render code style

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 
 import { lazy, StrictMode, Suspense } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 const App = lazy(() => import('./App'))
+const container = document.getElementById('root')
+const root = createRoot(container!)
 
-ReactDOM.render(
+root.render(
   <Suspense fallback={<div>loading...</div>}>
     <StrictMode>
       <App />
     </StrictMode>
-  </Suspense>,
-  document.getElementById('root')
+  </Suspense>
 )
